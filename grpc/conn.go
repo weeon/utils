123328745eb871276cc4f241b457e1882a56d6f2
orvice <orvice@gmail.com>
2019-05-12 14:20:57 +0800

    add listener from env func

--- grpc/conn.go
+++ grpc/conn.go
@@ -1,1 +1,14 @@
 package grpc
+
+import (
+	"net"
+	"os"
+)
+
+func ListenerFromEnv() (net.Listener, error) {
+	addr := os.Getenv("SRV_ADDR")
+	if len(addr) == 0 {
+		addr = ":8282"
+	}
+	return net.Listen("tcp", addr)
+}
