2d84c1363360ef21cbf2e215b7760c0e93ebc8dd
orvice <orvice@gmail.com>
2019-07-29 01:24:20 +0800

    add process

---
+++ process/process.go
@@ -0,0 +1,26 @@
+package process
+
+import (
+	"github.com/weeon/log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	exitFuncs = []func(){}
+)
+
+func AddExitFuncs(fns ...func()) {
+	exitFuncs = append(exitFuncs, fns...)
+}
+
+func WaitSignal() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGUSR2)
+	sig := <-sigs
+	log.Infof("recv signal %v", sig)
+	for _, fn := range exitFuncs {
+		fn()
+	}
+}
