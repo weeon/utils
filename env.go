4fb5cd64cfc9342db319e8624f5fc00d14c84c86
orvice <orvice@gmail.com>
2021-07-25 01:43:54 +0800

    add get env int

--- env.go
+++ env.go
@@ -1,6 +1,9 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func GetEvn(key string, df ...string) string {
 	v := os.Getenv(key)
@@ -8,4 +11,18 @@ func GetEvn(key string, df ...string) string {
 		return df[0]
 	}
 	return v
+}
+
+func GetEnvInt(key string, defaultValue ...int) int {
+	v := os.Getenv(key)
+	if len(v) == 0 {
+		return func() int {
+			if len(defaultValue) != 0 {
+				return defaultValue[0]
+			}
+			return 0
+		}()
+	}
+	i, _ := strconv.Atoi(v)
+	return i
 }
